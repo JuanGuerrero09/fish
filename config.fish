0e1bacd6ecad615da24479138ad15744aab508b1
juanguerrero09 <juanguerrero09mc@gmail.com>
2025-03-01 21:40:18 +0100

    config changed

--- config.fish
+++ config.fish
@@ -4,6 +4,10 @@ set -g PATH /home/linuxbrew/.linuxbrew/bin $PATH
 # Add Go to PATH
 set -g PATH /usr/local/go/bin $PATH
 
+function config
+    cd ~/.config/
+end
+
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
